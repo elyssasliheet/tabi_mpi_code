bb34865f6beda5fe29b9ffba6881938ce52eccf1
Elyssa Sliheet <esliheet@m3login04.cm.cluster>
2023-07-18 17:53:47 -0500

    plotting

--- plotting.py
+++ plotting.py
@@ -1,27 +1,18 @@
 # This program is used to plot CPU time for cyclic vs sequential runs
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 
 # Load in the data
-cyclic_data = pd.read_csv('tabipb-code-cyclic_precond/test_scripts/treecode_times_064.txt', header=None, delimiter=r"\s+")
-cyclic_data_np = np.loadtxt('tabipb-code-cyclic_precond/test_scripts/treecode_times_064.txt')
+# Note: EACH ROW is an iteration
+cyclic_data_np = np.loadtxt('tabipb-code-cyclic_precond/test_scripts/treecode_times_256.txt')
+sequential_data_np = np.loadtxt('tabipb-code-sequential_precond/test_scripts/treecode_times_256.txt')
 
-sequential_data = pd.read_csv('tabipb-code-sequential_precond/test_scripts/treecode_times_064.txt', header=None, delimiter=r"\s+")
-sequential_data_np = np.loadtxt('tabipb-code-sequential_precond/test_scripts/treecode_times_064.txt')
-#cyclic_data.columns = ['a', 'b', 'c', 'd', 'f']
-#sequential_data.columns = ['a', 'b', 'c', 'd', 'f']
+print(cyclic_data_np.shape)
 
-print(cyclic_data.head())
-
-#cyclic_data = cyclic_data.sort_values(by=['f'])
-#sequential_data = sequential_data.sort_values(by=['f'])
-#print(cyclic_data.head())
-
-#plt.plot(sequential_data.f, sequential_data.d, label = 'sequential')
-#plt.plot(cyclic_data.f, cyclic_data.d, label = 'cyclic')
-#plt.title('Sequential vs. Cyclic Parallelism')
-#plt.xlabel('MPI Task Index')
-#plt.ylabel('Computation Time (s)')
-#plt.legend()
-#plt.show()
+plt.plot(range(0,cyclic_data_np.shape[1]), cyclic_data_np[0,:], label = 'cyclic')
+plt.plot(range(0,cyclic_data_np.shape[1]), sequential_data_np[0,:], label = 'sequential')
+plt.title('Sequential vs. Cyclic Parallelism w/ Preconditioner')
+plt.xlabel('MPI Task Index')
+plt.ylabel('Computation Time (s)')
+plt.legend()
+plt.savefig('plot256_precond.png')
