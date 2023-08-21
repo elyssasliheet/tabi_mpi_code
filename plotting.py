48801942b7d18cbe973b71988179131008c06efe
Elyssa Sliheet <esliheet@m3login05.cm.cluster>
2023-08-21 15:24:58 -0500

    plot CPU times

--- plotting.py
+++ plotting.py
@@ -8,11 +8,14 @@ cyclic_data_np = np.loadtxt('tabipb-code-cyclic_precond/test_scripts/treecode_ti
 sequential_data_np = np.loadtxt('tabipb-code-sequential_precond/test_scripts/treecode_times_256.txt')
 
 print(cyclic_data_np.shape)
+print(sequential_data_np.shape)
 
-plt.plot(range(0,cyclic_data_np.shape[1]), cyclic_data_np[0,:], label = 'cyclic')
-plt.plot(range(0,cyclic_data_np.shape[1]), sequential_data_np[0,:], label = 'sequential')
-plt.title('Sequential vs. Cyclic Parallelism w/ Preconditioner')
-plt.xlabel('MPI Task Index')
-plt.ylabel('Computation Time (s)')
+print(cyclic_data_np[1,:])
+itr_num = 10
+plt.plot(range(0,cyclic_data_np.shape[1]), cyclic_data_np[1,:], linewidth = 2, marker = 'o', mfc='none', label = 'cyclic')
+plt.plot(range(0,cyclic_data_np.shape[1]), sequential_data_np[1,:], linewidth = 2, marker ='s', mfc='none', label = 'sequential')
+#plt.title('Sequential vs. Cyclic Parallelism w/ Preconditioner')
+plt.xlabel('CPU Index')
+plt.ylabel('CPU Time (s)')
 plt.legend()
 plt.savefig('plot256_precond.png')
